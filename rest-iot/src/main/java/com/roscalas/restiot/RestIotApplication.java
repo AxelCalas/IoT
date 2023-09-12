14598a86e0bc6fde78fc164cad0c460a7ea0f421
Axel Calas <axelcalas67@gmail.com>
2023-09-12 10:17:55 -0300

    add rest service

---
+++ rest-iot/src/main/java/com/roscalas/restiot/RestIotApplication.java
@@ -0,0 +1,13 @@
+package com.roscalas.restiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestIotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestIotApplication.class, args);
+	}
+
+}
---
+++ rest-iot/src/test/java/com/roscalas/restiot/RestIotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roscalas.restiot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestIotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
